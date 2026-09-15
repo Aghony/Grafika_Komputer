16fe5ad0a6d37e050a86070a80ba6945098f5dc2
aghony <kerenjustin286@gmail.com>
2026-09-15 22:35:14 +0700

    Updated Progress

---
+++ exercise/main.js
@@ -0,0 +1,8 @@
+function tampilkan(nama) {
+
+    // Sembuyi semua class
+    document.getElementById("").style.display = "none";
+
+    // Tampilkan canvas yang di pilih
+    document.getElementById(nama).style.display = "block";
+}
